051815bad71f1336e6a3893db417777dbce59947
Phil Powell <phil@feef.io>
2019-05-07 22:45:35 +0100

    Remove *Template imports from app

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import { CommandComponent } from './components/CommandComponent.js'
 import { NoteComponent } from './components/NoteComponent.js'
 import { PageComponent } from './components/PageComponent.js'
 import { Note } from './models/Note.js'
-import { PageTemplate } from './templates/PageTemplate.js';
-import { CommandTemplate } from './templates/CommandTemplate.js';
-import { NoteTemplate } from './templates/NoteTemplate.js';
 
 Application.name = 'twere'
 
