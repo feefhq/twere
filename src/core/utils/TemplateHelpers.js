2009befc43af52a58929624af35e0b6be1df98e4
Phil Powell <phil@feef.io>
2019-05-09 21:48:41 +0100

    Add some tets for TemplateHelper

--- src/core/utils/TemplateHelpers.js
+++ src/core/utils/TemplateHelpers.js
@@ -2,10 +2,6 @@
  * This is a POC for chainable template helper fucntions.
  */
 
-export const then = (action, execute) => {
-  return execute ? action : ``
-}
-
 export const when = condition => {
   return { then: (action) => then(action, condition) }
 }
@@ -14,10 +10,27 @@ export const has = condition => {
   return { then: (action) => then(action, condition && condition.length) }
 }
 
+/**
+ *
+ * @param {*} condition
+ */
 export const no = condition => {
   return { then: (action) => then(action, !condition || !condition.length) }
 }
 
+/**
+ *
+ * @param {*} iterable
+ */
 export const all = iterable => {
   return { then: (action) => then(iterable.map(action).join(''), true) }
+}
+
+/**
+ *
+ * @param {*} action
+ * @param {*} execute
+ */
+export const then = (action, execute) => {
+  return (action !== undefined && execute) ? action : ``
 }
---
+++ src/core/utils/TemplateHelpers.spec.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+import { then } from './TemplateHelpers.js'
+
+describe('Tempalte Helper - then()', () => {
+  it('Should execute if true', () => {
+    then('truthy', true).should.equal('truthy')
+  })
+
+  it('Should not execute if false', () => {
+    then('truthy', false).should.equal('')
+    then('truthy').should.equal('')
+  })
+
+  it('Should not execute if no args', () => {
+    then().should.equal('')
+  })
+
+  it('Should not execute if expression is undefined', () => {
+    then(undefined, true).should.equal('')
+    then(undefined, false).should.equal('')
+    then(undefined).should.equal('')
+  })
+})
+
+describe('Tempalte Helper - no()', () => {
+})
