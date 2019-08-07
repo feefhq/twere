b43f0b5dfa993ac1b16890d78e27e9d99a7c3925
Phil Powell <phil@feef.io>
2019-08-07 22:43:55 +0100

    Add Firefox to tests

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,7 @@ module.exports = function (config) {
     port: 9999,
     colors: true,
     logLevel: config.LOG_INFO,
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeHeadless', 'Firefox'],
     singleRun: true,
     concurrency: Infinity,
     preprocessors: {
