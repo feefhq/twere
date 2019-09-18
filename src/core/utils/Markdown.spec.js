828bfa97564a908a63073ba45285afcc9ed72955
Phil Powell <phil@feef.io>
2019-09-18 23:03:37 +0100

    Fix shameful typos in markdown tests

--- src/core/utils/Markdown.spec.js
+++ src/core/utils/Markdown.spec.js
@@ -23,7 +23,9 @@ describe('Markdown basic', () => {
     Markdown.toHTML('Starts here\n\n\n').should.equal('Starts here')
     Markdown.toHTML('\nStarts here\n').should.equal('Starts here')
     Markdown.toHTML('\n\nStarts here\n\n').should.equal('Starts here')
-    Markdown.toHTML('\n\nStarts here\n\nNew para\n\n').should.equal('<p>Starts here</p><p>New para</p>')
+    Markdown.toHTML('\n\nStarts here\n\nNew para\n\n').should.equal(
+      '<p>Starts here</p><p>New para</p>'
+    )
   })
 
   it('should convert **mighty** to <strong/>', () => {
@@ -31,11 +33,15 @@ describe('Markdown basic', () => {
   })
 
   it('should convert double line break to <p></p>', () => {
-    Markdown.toHTML('Paragraph 1\n\nPragraph 2').should.equal('<p>Paragraph 1</p><p>Pragraph 2</p>')
+    Markdown.toHTML('Paragraph 1\n\nPragraph 2').should.equal(
+      '<p>Paragraph 1</p><p>Pragraph 2</p>'
+    )
   })
 
   it('should convert double line break to <p></p> multiple times', () => {
-    Markdown.toHTML('Paragraph 1\n\nParagraph 2\n\nParagraph 3').should.equal('<p>Paragraph 1</p><p>Paragraph 2</p><p>Paragraph 3</p>')
+    Markdown.toHTML('Paragraph 1\n\nParagraph 2\n\nParagraph 3').should.equal(
+      '<p>Paragraph 1</p><p>Paragraph 2</p><p>Paragraph 3</p>'
+    )
   })
 
   it('should convert single line break to <br>', () => {
@@ -49,27 +55,40 @@ describe('Markdown code', () => {
   })
 
   it('should retain line breaks in code blocks', () => {
-    Markdown.toHTML('```\nSome code\n\nAnother line of code\n```').should.equal('<pre>Some code\n\nAnother line of code</pre>')
+    Markdown.toHTML('```\nSome code\n\nAnother line of code\n```').should.equal(
+      '<pre>Some code\n\nAnother line of code</pre>'
+    )
   })
 
-  it('shoudl convert `inline code` in middle of a para', () => {
-    Markdown.toHTML('A para with `some code` in it').should.equal('A para with <code>some code</code> in it')
+  it('should convert `inline code` in middle of a para', () => {
+    Markdown.toHTML('A para with `some code` in it').should.equal(
+      'A para with <code>some code</code> in it'
+    )
   })
 
-  it('shoudl convert `inline code` at start of a para', () => {
-    Markdown.toHTML('`some code` at start of a para').should.equal('<code>some code</code> at start of a para')
+  it('should convert `inline code` at start of a para', () => {
+    Markdown.toHTML('`some code` at start of a para').should.equal(
+      '<code>some code</code> at start of a para'
+    )
   })
 
-  it('shoudl convert `inline code` at end of a para', () => {
-    Markdown.toHTML('Para has code `at the end`').should.equal('Para has code <code>at the end</code>')
+  it('should convert `inline code` at end of a para', () => {
+    Markdown.toHTML('Para has code `at the end`').should.equal(
+      'Para has code <code>at the end</code>'
+    )
   })
 
-  it('shoudl convert `inline code` twice in a para', () => {
-    Markdown.toHTML('Para has code `here` and also `here`').should.equal('Para has code <code>here</code> and also <code>here</code>')
+  it('should convert `inline code` twice in a para', () => {
+    Markdown.toHTML('Para has code `here` and also `here`').should.equal(
+      'Para has code <code>here</code> and also <code>here</code>'
+    )
   })
 
-  it('shoudl convert `inline code` across paras', () => {
-    Markdown.toHTML('Para has code `here` and also `here`\n\n`And more code` in a second `para` hurrah!')
-      .should.equal('<p>Para has code <code>here</code> and also <code>here</code></p><p><code>And more code</code> in a second <code>para</code> hurrah!</p>')
+  it('should convert `inline code` across paras', () => {
+    Markdown.toHTML(
+      'Para has code `here` and also `here`\n\n`And more code` in a second `para` hurrah!'
+    ).should.equal(
+      '<p>Para has code <code>here</code> and also <code>here</code></p><p><code>And more code</code> in a second <code>para</code> hurrah!</p>'
+    )
   })
 })
