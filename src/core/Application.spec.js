ade48234d5d2c412cb85921a89514d02bab6199f
Phil Powell <phil@feef.io>
2019-09-18 22:50:40 +0100

    Bring experimental SW back into the mix

--- src/core/Application.spec.js
+++ src/core/Application.spec.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 import { Application } from './Application.js'
+import { DB } from './storage/DB.js'
 
 describe('Application', () => {
   it('should have default name', () => {
@@ -9,5 +10,12 @@ describe('Application', () => {
   it('should allow change of name', () => {
     Application.name = 'testname'
     Application.name.should.equal('testname')
+  })
+
+  it('should set a service worker', () => {
+  })
+
+  it('should return a DB instance', () => {
+    Application.db.should.be.instanceOf(DB)
   })
 })
