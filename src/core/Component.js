2b0bf93c8bc26046124d536ad150ee72e50c1bbb
Phil Powell <phil@feef.io>
2019-09-27 21:50:05 +0100

    Tidy Compoenent a little

--- src/core/Component.js
+++ src/core/Component.js
@@ -1,5 +1,6 @@
 import { EventMixin } from './mixins/EventMixin.js'
 import { Template } from './Template.js'
+import { Style } from './css/Style.js'
 
 /**
  * Base class for creating components. Based on the Custom Elements API.
@@ -8,9 +9,7 @@ import { Template } from './Template.js'
 export class Component extends EventMixin(window.HTMLElement) {
   /**
    * Invoked each time the element is appended into a document-connected element, as per
-   * the Custom Elements API. This is a bit of a black box callback. It's not part of
-   * a documented prototype; instead it gets added as part of a mutation, when calling
-   * `CustomElementRegistry.define()`.
+   * the Custom Elements API.
    *
    * Here we use it to do an initial paint, regardleess of state. Components are
    * expected to manage repaints as per their own criteria.
@@ -23,13 +22,18 @@ export class Component extends EventMixin(window.HTMLElement) {
    * HTML which is used to render the component, either as a simple `String` or as a
    * DocumentFragment. For convenience, a fragment can be set using the `Template.dom`
    * literal template tag.
-   * @returns {String|DocumentFragment}
    */
-  get html () { return '' }
-  set html (html) { super.html = html }
+  get html () {
+    return ''
+  }
+
+  set html (html) {
+    super.html = html
+  }
 
   /**
-   * Paint the element into the DOM
+   * Paint the element into the DOM. Shadow DOM is not currently used, because CSS
+   * styles are not yet abstracted properly.
    * @param {string|DocumentFragment} html
    */
   paint (html) {
