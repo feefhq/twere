4fc3c6f94b29117014e1abe8156235a58ed2d94d
Phil Powell <phil@feef.io>
2019-12-03 21:27:31 +0000

    Add basic components and linting

---
+++ src/Prompt.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Prompt.css'
+
+class Prompt extends React.Component {
+  render () {
+    return <div className='Prompt'>Prompt</div>
+  }
+}
+
+export default Prompt
