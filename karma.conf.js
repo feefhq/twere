5340c64a5eeb9db136950a2fd0374a02ecf07f08
Phil Powell <phil@feef.io>
2019-12-17 20:45:48 +0000

    Tidy up testing config

--- karma.conf.js
+++ karma.conf.js
@@ -1,46 +1,55 @@
 // Karma configuration
-// const path = require('path')
+const path = require('path')
+
+const coverage = process.argv.find(arg => arg.includes('--coverage'))
 
 module.exports = function (config) {
   config.set({
-    captureTimeout: 300000,
-    browserNoActivityTimeout: 300000,
-    browserDisconnectTolerance: 2,
+    browsers: ['ChromeHeadlessNoSandbox'],
+
     customLaunchers: {
       FirefoxHeadless: {
         base: 'Firefox',
         flags: ['-headless']
       },
       ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless'
-        // flags: ['--no-sandbox']
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-setuid-sandbox']
       }
     },
-    frameworks: ['mocha', 'chai', 'sinon'],
-    files: [
-      { pattern: 'src/**/!(index).js', type: 'module' }
-    ],
-    reporters: ['mocha'],
-    port: 9999,
-    // Using a different launcher until this issue is resolved
-    // https://github.com/karma-runner/karma-safari-launcher/issues/29
-    browsers: ['ChromeHeadlessNoSandbox'],
 
-    // Disabling coverage reporting for now, as it's proving tricky to set up multiple Karma configs.
-    // preprocessors: {
-    //   'src/**/!(*.spec).js': ['karma-coverage-istanbul-instrumenter']
-    // },
-    // coverageIstanbulInstrumenter: {
-    //   esModules: true
-    // },
-    // coverageIstanbulReporter: {
-    //   reports: ['lcov', 'text'],
-    //   dir: path.join(__dirname, 'coverage')
-    // },
-    restartOnFileChange: true,
+    frameworks: ['mocha', 'chai', 'sinon'],
+
+    preprocessors: {
+      'src/**/!(*.spec).js': ['karma-coverage-istanbul-instrumenter']
+    },
+
+    reporters: coverage ? ['mocha', 'coverage-istanbul'] : ['mocha'],
+
     mochaReporter: {
       output: 'autowatch',
       showDiff: true
-    }
+    },
+
+    restartOnFileChange: true,
+
+    colors: true,
+
+    coverageIstanbulInstrumenter: {
+      esModules: true
+    },
+
+    coverageIstanbulReporter: {
+      reports: ['lcov', 'text'],
+      dir: path.join(__dirname, 'coverage')
+    },
+
+    autoWatch: false,
+    singleRun: true,
+    concurrency: Infinity,
+
+    files: [
+      { pattern: 'src/**/!(index).js', type: 'module' }
+    ]
   })
 }
