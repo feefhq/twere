16f15de447f1101a533ceff2bd041e6a3df708ab
Phil Powell <phil@feef.io>
2020-01-07 21:56:52 +0000

    Start to add design system

--- src/components/palette/TextInput.js
+++ src/components/palette/TextInput.js
@@ -2,13 +2,16 @@ import React from 'react'
 import styles from './TextInput.module.css'
 
 class TextInput extends React.Component {
-  handleClick = () => {
-    console.log('Clicked:', this)
+  constructor (props) {
+    super(props)
+    this.state = {
+      text: props.text
+    }
   }
 
   render () {
     return (
-      <textarea className={styles.textarea} onClick={this.handleClick} rows='1' />
+      <textarea className={styles.TextInput} disabled={this.props.disabled} rows='1'>{this.state.text}</textarea>
     )
   }
 }
--- src/components/palette/TextInput.stories.js
+++ src/components/palette/TextInput.stories.js
@@ -11,3 +11,15 @@ export default {
 export const Default = () => {
   return <TextInput />
 }
+
+export const Prepopulated = () => {
+  return <TextInput text='Some default text content.' />
+}
+
+export const PrepopulatedMultiline = () => {
+  return <TextInput text='Some default text content. This is designed to overflow so that the input can handle multiple lines of content.' />
+}
+
+export const Disabled = () => {
+  return <TextInput text='This text should not be editable.' disabled />
+}
