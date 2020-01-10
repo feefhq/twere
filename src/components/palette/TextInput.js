0fbb9fb1d7f482d6936240728a26b55722375f89
Phil Powell <phil@feef.io>
2020-01-10 18:58:15 +0000

    Add additional states to TextInput

--- src/components/palette/TextInput.js
+++ src/components/palette/TextInput.js
@@ -4,15 +4,37 @@ import styles from './TextInput.module.css'
 class TextInput extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {
-      text: props.text
-    }
+    this.state = { value: props.value }
+    this.textareaRef = React.createRef()
+  }
+
+  componentDidMount () {
+    this.resize()
+  }
+
+  handleChange (e) {
+    this.setState({ [e.target.name]: e.target.value })
+    this.resize()
   }
 
   render () {
     return (
-      <textarea className={styles.TextInput} disabled={this.props.disabled} rows='1'>{this.state.text}</textarea>
+      <textarea
+        name='value'
+        ref={this.textareaRef}
+        value={this.state.value}
+        onChange={e => this.handleChange(e)}
+        className={styles.TextInput}
+        disabled={this.props.disabled}
+        rows='1'
+      />
     )
+  }
+
+  resize () {
+    const textarea = this.textareaRef.current
+    textarea.style.height = 'auto'
+    textarea.style.height = `${textarea.scrollHeight}px`
   }
 }
 
--- src/components/palette/TextInput.stories.js
+++ src/components/palette/TextInput.stories.js
@@ -13,13 +13,17 @@ export const Default = () => {
 }
 
 export const Prepopulated = () => {
-  return <TextInput text='Some default text content.' />
+  return <TextInput value='Some default text content.' />
 }
 
 export const PrepopulatedMultiline = () => {
-  return <TextInput text='Some default text content. This is designed to overflow so that the input can handle multiple lines of content.' />
+  return <TextInput value='Some default text content. This is designed to overflow so that the input can handle multiple lines of content.' />
 }
 
 export const Disabled = () => {
-  return <TextInput text='This text should not be editable.' disabled />
+  return <TextInput value='This text should not be editable.' disabled />
+}
+
+export const DisabledMultiline = () => {
+  return <TextInput value='This text should not be editable. And should resize when it overflows due to blah de blah de blah. Resizing should happen regardless of state.' disabled />
 }
